9e2a9f8c197b6de0a4d307af80bd064c7a975d84
isami-odagiri-spiderplus <isami.odagiri@spiderplus.co.jp>
2025-01-30 17:57:50 +0900

    feat: issueが出るように変更

--- codacy-Test/codacy-Test/ContentView.swift
+++ codacy-Test/codacy-Test/ContentView.swift
@@ -45,6 +45,28 @@ extension ContentView {
         }
         print("\(count)")
     }
+    
+    func testMethod2(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {
+        if a == 0 {
+            if b == 1 {
+                if c == 2 {
+                    if d == 3 {
+                        print("aaa")
+                    } else {
+                        print("vvv")
+                    }
+                    
+                    if e == 0 {
+                        if f == 0 {
+                            if g == 0 {
+                                print("vvv")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 #Preview {
