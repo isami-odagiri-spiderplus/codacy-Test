7e42cdb235de354d3b312bb3de6e207eea50d247
isami-odagiri-spiderplus <isami.odagiri@spiderplus.co.jp>
2025-01-30 12:42:35 +0900

    feat: お試しでメソッド追加

--- codacy-Test/codacy-Test/ContentView.swift
+++ codacy-Test/codacy-Test/ContentView.swift
@@ -19,6 +19,27 @@ struct ContentView: View {
     }
 }
 
+extension ContentView {
+    func testMethod() {
+        var count = 0
+        if count == 0 {
+            count += 1
+            if count == 1 {
+                count += 1
+                if count == 2 {
+                    count += 1
+                    if count == 3 {
+                        count += 1
+                    } else {
+                        count -= 1
+                    }
+                }
+            }
+        }
+        print("\(count)")
+    }
+}
+
 #Preview {
     ContentView()
 }
