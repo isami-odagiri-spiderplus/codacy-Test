81750a1ed7012ae351d86f73ce360d564519e01d
isami-odagiri-spiderplus <isami.odagiri@spiderplus.co.jp>
2025-01-31 09:29:10 +0900

    feat: issueが出るように変更

--- codacy-Test/codacy-Test/test_NameWorning.swift
+++ codacy-Test/codacy-Test/test_NameWorning.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 final class test_NameWorning {
-    
+    func testMethod2(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) {
+        if a == 0 {
+            if b == 1 {
+                if c == 2 {
+                    if d == 3 {
+                        print("aaa")
+                    } else {
+                        print("vvv")
+                    }
+                    
+                    if e == 0 {
+                        if f == 0 {
+                            if g == 0 {
+                                print("vvv")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
